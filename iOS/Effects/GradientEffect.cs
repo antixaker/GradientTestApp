70b6e3468cb2934785e8c12b7a8824b34c103fd7
antixaker <proskura-s@mail.ru>
2016-02-08 18:04:33 +0200

    Finish radial gradient for iOS and disable page renderer

--- iOS/Effects/GradientEffect.cs
+++ iOS/Effects/GradientEffect.cs
@@ -3,31 +3,82 @@ using Xamarin.Forms.Platform.iOS;
 using CoreAnimation;
 using CoreGraphics;
 using Xamarin.Forms;
+using TestGradient.Views;
 using TestGradient.iOS.Effects;
+using UIKit;
 
 [assembly: ResolutionGroupName("TestEffect")]
-[assembly: ExportEffect(typeof(GradientEffectIOS), "GradientEffect")]
+[assembly: ExportEffect(typeof(GradientEffect), "GradientEffect")]
 namespace TestGradient.iOS.Effects
 {
-    public class GradientEffectIOS : PlatformEffect
+    public class GradientEffect : PlatformEffect
     {
-        Xamarin.Forms.Color StartColor { get; set; } = Xamarin.Forms.Color.Yellow;
-
-        Xamarin.Forms.Color EndColor { get; set; } = Xamarin.Forms.Color.Green;
+        GradientView currentView;
 
         protected override void OnAttached()
         {
-            var gradientLayer = new CAGradientLayer();
-            //                gradientLayer.AffineTransform = CGAffineTransform.MakeRotation (new nfloat(45.0));
-            gradientLayer.Frame = Container.Bounds;
-            gradientLayer.Colors = new CGColor[] { StartColor.ToCGColor(), EndColor.ToCGColor() };
-            Container.Layer.InsertSublayer(gradientLayer, 0);
+            currentView = this.Element as GradientView;
+            if (currentView != null)
+            {
+                currentView.SizeChanged += Renderer_Element_SizeChanged;
+            }
+            else
+            {
+                throw new NotSupportedException("Element doesn't support this type of effect. You must inherit element from GradientView.");
+            }
+
+        }
+
+        void Renderer_Element_SizeChanged(object sender, EventArgs e)
+        {
+            var element = sender as GradientView;
+            if (element == null)
+                return;
+            
+            var bounds = element.Bounds;
+            var rect = new CGRect(bounds.X, bounds.Y, bounds.Width, bounds.Height);
+
+            UIGraphics.BeginImageContext(rect.Size);
+            using (CGContext g = UIGraphics.GetCurrentContext())
+            {
+                using (var rgb = CGColorSpace.CreateDeviceRGB())
+                {
+                    nfloat[] colors = GetFloatArrayFromColors(element.StartColor, element.EndColor);
+
+                    var gradient = new CGGradient(rgb, colors, null);
+
+                    g.DrawRadialGradient(gradient, new CGPoint(0, 0), new nfloat(0), new CGPoint(100, 100), new nfloat(1000), CGGradientDrawingOptions.None);
+                }
+
+                UIImage im = UIGraphics.GetImageFromCurrentImageContext();
+                UIGraphics.EndImageContext();
+
+                var imageHolder = new UIImageView(im);
+                Container.AddSubview(imageHolder);
+            }
 
         }
 
         protected override void OnDetached()
         {
-            throw new NotImplementedException();
+            currentView.SizeChanged -= Renderer_Element_SizeChanged;
+        }
+
+        nfloat[] GetFloatArrayFromColors(Color startColor, Color endColor)
+        {
+            var array = new nfloat[8];
+
+            array[0] = new nfloat(startColor.R);            
+            array[1] = new nfloat(startColor.G);
+            array[2] = new nfloat(startColor.B);
+            array[3] = new nfloat(startColor.A);
+
+            array[4] = new nfloat(endColor.R);            
+            array[5] = new nfloat(endColor.G);
+            array[6] = new nfloat(endColor.B);
+            array[7] = new nfloat(endColor.A);
+
+            return array;
         }
     }
 }
--- iOS/Renderers/GradientRendererIOS.cs
+++ iOS/Renderers/GradientRendererIOS.cs
@@ -6,7 +6,7 @@ using CoreGraphics;
 using Xamarin.Forms;
 using Renderers;
 
-[assembly: ExportRenderer(typeof(GradientPage), typeof(GradientContentPageRenderer))]
+//[assembly: ExportRenderer(typeof(GradientPage), typeof(GradientContentPageRenderer))]
 namespace Renderers
 {
     public class GradientContentPageRenderer : PageRenderer
@@ -17,12 +17,12 @@ namespace Renderers
 
             if (e.OldElement == null) // perform initial setup
             {
-                var page = e.NewElement as GradientPage;
-                var gradientLayer = new CAGradientLayer();
-//                gradientLayer.AffineTransform = CGAffineTransform.MakeRotation (new nfloat(45.0));
-                gradientLayer.Frame = View.Bounds;
-                gradientLayer.Colors = new CGColor[] { page.StartColor.ToCGColor(), page.EndColor.ToCGColor() };
-                View.Layer.InsertSublayer(gradientLayer, 0);
+//                var page = e.NewElement as GradientPage;
+//                var gradientLayer = new CAGradientLayer();
+////                gradientLayer.AffineTransform = CGAffineTransform.MakeRotation (new nfloat(45.0));
+//                gradientLayer.Frame = View.Bounds;
+//                gradientLayer.Colors = new CGColor[] { page.StartColor.ToCGColor(), page.EndColor.ToCGColor() };
+//                View.Layer.InsertSublayer(gradientLayer, 0);
             }
         }
     }
