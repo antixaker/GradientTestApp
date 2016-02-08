2b07c390c43005f1cf75a3e5b9c3f8afff76c7bf
antixaker <proskura-s@mail.ru>
2016-02-08 13:33:03 +0200

    Delete useless code and files

--- TestGradient/GradientPage.xaml.cs
+++ TestGradient/GradientPage.xaml.cs
@@ -11,8 +11,6 @@ namespace TestGradient
         {
             InitializeComponent();
 
-//            leibe.Effects.Add(Effect.Resolve("TestEffect.GradientEffect"));
-//            buton.Effects.Add(Effect.Resolve("TestEffect.GradientEffect"));
             viewForEffect.Effects.Add(Effect.Resolve("TestEffect.GradientEffect"));
         }
 
