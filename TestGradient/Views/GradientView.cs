134a166a174c28105862fb1ddfee1936f3ab5b17
antixaker <proskura-s@mail.ru>
2016-02-09 16:24:47 +0200

    Simplify GradientView class (delete useless constructors)

--- TestGradient/Views/GradientView.cs
+++ TestGradient/Views/GradientView.cs
@@ -5,20 +5,9 @@ namespace TestGradient.Views
 {
     public class GradientView : View
     {
-        public GradientView()
-        {
-        }
-
-        public GradientView(Color first, Color second)
-        {
-            StartColor = first;
-            EndColor = second;
-        }
-
         public Color StartColor { get; set; }
 
         public Color EndColor { get; set; }
-
     }
 }
 
